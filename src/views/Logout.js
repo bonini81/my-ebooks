876931e35a3aa3372c16a61d676b6678bc9e0a1e
bonini81 <andresdominguez81@gmail.com>
2020-12-18 18:49:10 -0500

    Trying redirections on login and after logout

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,15 +1,18 @@
 import {useContext} from 'react';
+import { useHistory } from 'react-router-dom';
 import {AuthContext}  from '../contexts/AuthContext';
 
 
 const Logout = (props) => {
 
+    const history = useHistory();
     const { removeToken } = useContext(AuthContext);
     removeToken();
     alert('Sucessful logout');
+    history.push('/');
  
-
 }
 
 
 export default Logout;
+
